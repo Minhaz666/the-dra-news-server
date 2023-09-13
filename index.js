9ed9c5a836c9a02b4b857273646b889e1aa51d6f
unknown <labibmahodi.bd@gmail.com>
2023-09-14 04:24:36 +0600

    vercel config implemetatin

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const cors=require('cors');
 app.use(cors());
 
 const categories= require('./data/categories.json');
+const news=require('./data/news.json')
+// console.log(data)
 
 app.get('/', (req, res) => {
   res.send('news server is running')
@@ -14,6 +16,31 @@ app.get('/', (req, res) => {
 app.get('/categories', (req, res) => {
   res.send(categories)
 });
+
+app.get('/news', (req, res) => {
+  res.send(news);
+});
+
+app.get('/news/:id', (req, res) => {
+  const id= parseInt(req.params.id);
+  console.log (id)
+  const selectednews=news.find(singlenews=>parseInt(singlenews._id )== id)
+  res.send(selectednews);
+});
+
+app.get('/categories/:id',(req,res)=>{
+  const categoriesid=parseInt(req.params.id);
+  // console.log(name);
+  if (categoriesid===0)
+  {
+    res.send(news)
+  }
+  else{
+    const categoryNews=news.filter(singlenews=>categoriesid == parseInt(singlenews.category_id));
+  res.send(categoryNews)
+  }
+  
+})
 
 app.listen(port, () => {
   console.log(`news server is listening on port ${port}`)
